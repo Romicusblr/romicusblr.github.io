db923f4f3dad3978cc087e011b534d77dfd6bfd8
Raman <raman.mastyka@gmail.com>
2020-05-22 14:02:22 +0300

    add minsk build

---
+++ minsk/precache-manifest.68d3fcb00eff8be995a0945867f43e02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b675eb0e9e206ab6387a9ee4779ed60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91872515daf4b9d3c1b6",
+    "url": "/static/css/main.69f77828.chunk.css"
+  },
+  {
+    "revision": "c90f9f0ff65b808ed785",
+    "url": "/static/js/2.11f4ee7f.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/static/js/2.11f4ee7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91872515daf4b9d3c1b6",
+    "url": "/static/js/main.a6409e35.chunk.js"
+  },
+  {
+    "revision": "b0c7facf7b1bece052f2",
+    "url": "/static/js/runtime-main.39a31560.js"
+  }
+]);
